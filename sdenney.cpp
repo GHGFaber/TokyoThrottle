7a9433f1ecb1c107cbb2a0342f8bdce77ab02e55
loporean <84533280+loporean@users.noreply.github.com>
2022-10-04 09:52:27 -0700

    lab 7

--- sdenney.cpp
+++ sdenney.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "myImage.h"
+#include "fonts.h"
+#include "log.h"
+#include <X11/keysym.h>
+#include <GL/gl.h>
+#include <GL/glx.h>
+#include <GL/glu.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void show_name_s()
 {
 	printf("sdenney hi\n");
+}
+
+void pause()
+{
+	//
 }
